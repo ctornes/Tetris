b7a2059e7dde6ecf9cb7793dee9a104aaecf1804
Calogero Tornese <ctornes@gmail.com>
2023-06-21 18:26:48 +0200

    inizializzazione variabili javascript

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const larghezza = 10;
+    const grid = document.querySelector('.grid');
+    let quadrati = Array.from(document.querySelectorAll('.grid div'));
+
+    console.log(quadrati);
 
 });
+
